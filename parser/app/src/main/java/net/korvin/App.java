4687ffce9074f3653df1bd3e12e387745676eba8
Alan Toy <weblogic@bk.ru>
2022-01-08 01:17:23 +0300

    working example

--- parser/app/src/main/java/net/korvin/App.java
+++ parser/app/src/main/java/net/korvin/App.java
@@ -3,9 +3,12 @@
  */
 package net.korvin;
 
+import org.apache.commons.lang3.StringUtils;
+
 public class App {
     public String getGreeting() {
-        return "Hello World!";
+        return StringUtils.upperCase("hello world and sascha!");
+
     }
 
     public static void main(String[] args) {
